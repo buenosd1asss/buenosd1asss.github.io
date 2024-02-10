11284dcb5961c2172f086f5e00cadcc664b710fa
buenosd1asss <159629112+buenosd1asss@users.noreply.github.com>
2024-02-10 19:58:27 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function shrinkNo() {
+  var noBtn = document.getElementById('noBtn');
+  var yesBtn = document.getElementById('yesBtn');
+  var currentFontSize = parseInt(window.getComputedStyle(noBtn, null).getPropertyValue('font-size'));
+  if (currentFontSize > 10) {
+    noBtn.style.fontSize = (currentFontSize - 2) + 'px';
+    yesBtn.style.fontSize = (currentFontSize * 2) + 'px';
+  }
+}
+function enlargeYes() {
+  var yesBtn = document.getElementById('yesBtn');
+  var currentFontSize = parseInt(window.getComputedStyle(yesBtn, null).getPropertyValue('font-size'));
+  yesBtn.style.fontSize = (currentFontSize * 1.5) + 'px'; // Увеличиваем размер кнопки "Да"
+}
+
+function showMessage(message) {
+  alert(message);
+}
